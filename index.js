110218eae1a9d9b48891f33fa43f68fb317a1377
M6K4N9R <zubchevskiy@gmail.com>
2024-04-19 13:30:46 +0200

    Add calculation to characters left

--- index.js
+++ index.js
@@ -5,21 +5,32 @@ const form = document.querySelector('[data-js="form"]');
 const userQuestionInput = document.querySelector(
   '[data-js="user-question-input"]'
 );
-const userAnswerInput = document.querySelector(
-  '[data-js="user-answer-input"]'
+console.log(userQuestionInput);
+const userAnswerInput = document.querySelector('[data-js="user-answer-input"]');
+const characterLeftQuestion = document.querySelector(
+  '[data-js="remaining-characters-question"]'
 );
+const characterLeftAnswer = document.querySelector(
+  '[data-js="remaining-characters-answer"]'
+);
+const MAX_LENGTH = 150;
 
-
-form.addEventListener("submit", (event) => {
+const onSubmit = (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
   const data = Object.fromEntries(formData);
 
   console.log(data);
+  event.target.reset();
+};
+
+
+userQuestionInput.addEventListener("input", (e) => {
+  characterLeftQuestion.textContent = MAX_LENGTH - e.target.value.length;
+});
+userAnswerInput.addEventListener("input", (e) => {
+  characterLeftAnswer.textContent = MAX_LENGTH - e.target.value.length;
 });
 
-const inputCharactersLeft = document.createElement('p');
-inputCharactersLeft.classList.add("remaining-characters");
-
-
+form.addEventListener("submit", onSubmit);
