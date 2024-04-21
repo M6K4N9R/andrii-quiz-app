048f8dd08728aa09922a9c82b2526cfc2603dff9
M6K4N9R <zubchevskiy@gmail.com>
2024-04-21 18:39:38 +0200

    Before separating index.js into two

--- index.js
+++ index.js
@@ -12,35 +12,35 @@
 
 /*------- Toggle Button with toggle Attribute-----------------------------------------*/
 
-// const bookmarkButton = document.querySelector(
-//   '[data-js="bookmark--toggle-button"]'
-// );
-// const toggledImage = document.querySelector('[data-js="bookmark--toggle-img"]');
+const bookmarkButton = document.querySelector(
+  '[data-js="bookmark--toggle-button"]'
+);
+const toggledImage = document.querySelector('[data-js="bookmark--toggle-img"]');
 
-// bookmarkButton.addEventListener("click", () => {
-//   const currentSource = toggledImage.getAttribute("src");
-//   const newSource = currentSource.includes("bookmark_transparent.png")
-//     ? "./resources/bookmark_filled.png"
-//     : "./resources/bookmark_transparent.png";
+bookmarkButton.addEventListener("click", () => {
+  const currentSource = toggledImage.getAttribute("src");
+  const newSource = currentSource.includes("bookmark_transparent.png")
+    ? "./resources/bookmark_filled.png"
+    : "./resources/bookmark_transparent.png";
 
-//   toggledImage.setAttribute("src", newSource);
-// });
+  toggledImage.setAttribute("src", newSource);
+});
 
-// /*------- Show Answer Text Feature --------------------------------------------------*/
+/*------- Show Answer Text Feature --------------------------------------------------*/
 
-// const showAnswerButton = document.querySelector(
-//   '[data-js="button--answer--hidden"]'
-// );
-// const showAnswerText = document.querySelector('[data-js="answer-text-hidden"]');
+const showAnswerButton = document.querySelector(
+  '[data-js="button--answer--hidden"]'
+);
+const showAnswerText = document.querySelector('[data-js="answer-text-hidden"]');
 
-// showAnswerButton.addEventListener("click", () => {
-//   showAnswerText.classList.toggle("answer-text-hidden");
-//   showAnswerButton.textContent = showAnswerButton.textContent.includes(
-//     "Show answer"
-//   )
-//     ? "Hide answer"
-//     : "Show answer";
-// });
+showAnswerButton.addEventListener("click", () => {
+  showAnswerText.classList.toggle("answer-text-hidden");
+  showAnswerButton.textContent = showAnswerButton.textContent.includes(
+    "Show answer"
+  )
+    ? "Hide answer"
+    : "Show answer";
+});
 
 /* ========================= Form Page ==========================*/
 
